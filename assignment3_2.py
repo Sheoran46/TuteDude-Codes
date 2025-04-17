4a53749d5acb37d398eb0756f23dc0a5855cf150
Sheoran46 <88sheoran@gmail.com>
2025-04-17 20:17:54 +0530

    Add files via upload

---
+++ assignment3_2.py
@@ -0,0 +1,8 @@
+from math import *
+n=int(input("enter a number: "))
+square_root=sqrt(n)
+Logarithmic=log(n)
+sine=sin(n)
+print("square root: ",square_root)
+print("Logarithmic: ",Logarithmic)
+print("sine: ",sine)
